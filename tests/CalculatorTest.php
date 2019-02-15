25b5e475c366186d0dbd24837281162c88701e69
juan <bixmail12@hotmail.com>
2019-02-15 21:32:02 +0100

    cambios phpunit3

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -4,39 +4,38 @@ require 'Calculadora.php';
 class CalculatorTests extends TestCase
 {
     private $calculator;
- 
-    public static function setUpBeforeClass()
-    {
-        $this->calculator = new calculadora();
-    }
- 
-    public static function tearDownAfterClass()
-    {
-        $this->calculator = NULL;
-    }
- 
+  
     public function testAdd()
     {
+$this->calculator = new calculadora();
         $result = $this->calculator->sumar(1, 2);
         $this->assertEquals(3, $result);
+$this->calculator = NULL;
     }
     public function testMultiply()
     {
+$this->calculator = new calculadora();
         $result = $this->calculator->multiplicar(5, 2);
         $this->assertEquals(10, $result);
+$this->calculator = NULL;
     }
     public function testDivision()
     {
+$this->calculator = new calculadora();
         $result = $this->calculator->dividir(10, 2);
         $this->assertEquals(5, $result);
+$this->calculator = NULL;
     }
  public function testSubstration()
     {
+$this->calculator = new calculadora();
         $result = $this->calculator->restar(10, 2);
         $this->assertEquals(8, $result);
+$this->calculator = NULL;
     }
 public function testOperacion()
     {
+$this->calculator = new calculadora();
         $result = $this->calculator->operacion(10, 2,'suma');
         $this->assertEquals(12, $result);
         $result = $this->calculator->operacion(10, 2,'resta');
@@ -45,7 +44,7 @@ public function testOperacion()
         $this->assertEquals(20, $result);
         $result = $this->calculator->operacion(10, 2,'divide');
         $this->assertEquals(5, $result);
-
+$this->calculator = NULL;
 
     }
 }
